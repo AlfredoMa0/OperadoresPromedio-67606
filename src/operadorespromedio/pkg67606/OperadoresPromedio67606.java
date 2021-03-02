0239c48609a45fae148c49dabd01bbf374937800
Campe <Campe@LAPTOP-F5OP52OL>
2021-03-01 22:27:05 -0600

    Promedio

---
+++ src/operadorespromedio/pkg67606/OperadoresPromedio67606.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadorespromedio.pkg67606;
+
+/**
+ *
+ * @author Campe
+ */
+public class OperadoresPromedio67606 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor = 10;
+        int v1 = 4;
+        int v2 = 12;
+        int v3 =15;
+        int v4 =8;
+        int suma;
+        int promedio;
+        suma = valor+v1+v2+v3+v4;
+        promedio = suma / 5;
+        System.out.println("LA SUMA ES: " + suma);
+        System.out.println("EL PROMEDIO ES: " + promedio);
+    }
+    
+}
